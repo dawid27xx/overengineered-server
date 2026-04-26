ab7af23626f5dde6389c0995f2f928594582f1a2
dawid27xx <dawidstepien27@gmail.com>
2026-04-26 17:39:22 +0100

    Type Checking and gunicorn

--- tests.py
+++ tests.py
@@ -1,26 +1,29 @@
 import pytest
+from collections.abc import Generator
+from flask.testing import FlaskClient
+
 from server import app
 
 
 @pytest.fixture
-def client():
+def client() -> Generator[FlaskClient, None, None]:
     with app.test_client() as client:
         yield client
 
 
-def test_sample_get(client):
+def test_sample_get(client: FlaskClient) -> None:
     response = client.get('/sampleGet')
     assert response.status_code == 200
     assert response.data.decode() == 'Hello, World!'
 
 
-def test_sample_get2(client):
+def test_sample_get2(client: FlaskClient) -> None:
     response = client.get('/sampleGet2')
     assert response.status_code == 200
     assert response.data.decode() == 'Hello, World2!'
 
 
-def test_sample_post(client):
+def test_sample_post(client: FlaskClient) -> None:
     response = client.post('/samplePost', json={'key': 'value'})
     assert response.status_code == 200
     assert response.get_json() == {'received': {'key': 'value'}}
